df9fcca75de7d1ff6b3b1ac90ba88c4ce635edbf
umakusumuru <kusumuruuma@gmail.com>
2017-09-18 10:47:02 +0530

    reverted single db hit

--- dist/src/main.js
+++ dist/src/main.js
@@ -1457,7 +1457,7 @@ regApp.factory('getArray', ['arrayConstantsreg', 'SelectBindServicereg', 'countr
                         //     option = option;
                         // });
 
-                        angular.copy(countryArrayModel.ProfCatgory, option);
+                        angular.copy(eduprofArrayModel.ProfCatgory, option);
 
                         // option = eduprofArrayModel.ProfCatgory;
                         if (option[0] !== undefined && angular.lowercase(option[0].title) === '--select--') {
@@ -1476,7 +1476,7 @@ regApp.factory('getArray', ['arrayConstantsreg', 'SelectBindServicereg', 'countr
                         //     option = option;
                         // });
 
-                        angular.copy(countryArrayModel.ProfGroup, option);
+                        angular.copy(eduprofArrayModel.ProfGroup, option);
                         // option = eduprofArrayModel.ProfGroup;
                         if (option[0] !== undefined && angular.lowercase(option[0].title) === '--select--') {
                             option.splice(0, 1);
@@ -1527,7 +1527,7 @@ regApp.factory('getArray', ['arrayConstantsreg', 'SelectBindServicereg', 'countr
                         //     });
                         //     option = option;
                         // });
-                        angular.copy(countryArrayModel.caste, option);
+                        angular.copy(otherArrayModel.caste, option);
 
                         // option = otherArrayModel.caste;
                         if (option[0] !== undefined && angular.lowercase(option[0].title) === '--select--') {
