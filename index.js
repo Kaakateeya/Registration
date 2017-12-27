e515cbb6a1a2c1c6a818678ce20624204a3a1029
lakshmidevi407 <lakshmidevi407@gmail.com>
2017-12-27 11:01:25 +0530

    27_12_2017_chats

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 var regApp = angular.module('KaakateeyaRegistration', ['ui.router', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ngMaterial', 'ngMessages', 'ngAria', 'ngMdIcons', 'ngPassword', 'jcs-autoValidate']);
 // regApp.apipath = 'http://54.169.133.223:8070/Api/';
 // regApp.apipath = 'http://183.82.0.58:8010/Api/';
-regApp.apipath = 'http://183.82.0.58:8010/Api/';
+regApp.apipath = 'http://183.82.0.58:8070/Api/';
 
 
 
